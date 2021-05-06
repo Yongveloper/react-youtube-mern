8d84de714c20f37f2f199239b3b88ef58d14e0aa
Yongveloper <aaa9015@naver.com>
2021-05-06 23:01:48 +0900

    #4 비디오 업로드 Form 만들기2

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -37,7 +37,7 @@ function RightMenu(props) {
           <Link to="/video/upload">Video</Link>
         </Menu.Item>
         <Menu.Item key="logout">
-          <Link onClick={logoutHandler}>Logout</Link>
+          <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
       </Menu>
     );
--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, Button, Form, Message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+  { value: 0, label: '비공개' },
+  { value: 1, label: '공개' },
+];
+
+const CategoryOptions = [
+  { value: 0, label: '영화 및 애니메이션' },
+  { value: 1, label: '자동차 및 차량' },
+  { value: 2, label: '음악' },
+  { value: 3, label: '애완동물 및 동물' },
+];
+
 function VideoUploadPage() {
+  const [videoTitle, setVideoTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [videoPrivate, setVideoPrivate] = useState(0);
+  const [category, setCategory] = useState('애완동물 및 동물');
+
+  const onTitleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setVideoTitle(value);
+  };
+
+  const onDiscriptionChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setDescription(value);
+  };
+
+  const onPrivateChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setVideoPrivate(value);
+  };
+
+  const onCategoryChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setCategory(value);
+  };
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -41,20 +86,28 @@ function VideoUploadPage() {
         <br />
         <br />
         <label>제목</label>
-        <Input onChange value />
+        <Input onChange={onTitleChange} value={videoTitle} />
         <br />
         <br />
         <label>설명</label>
-        <TextArea onChange value />
+        <TextArea onChange={onDiscriptionChange} value={description} />
         <br />
         <br />
-        <select>
-          <option key="1" onChange></option>
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
-        <select>
-          <option key="1" onChange></option>
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
