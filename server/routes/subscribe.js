bef3573e022537da4423963a4710157b57929924
Yongveloper <aaa9015@naver.com>
2021-06-07 00:23:11 +0900

    #13 구독 비디오 페이지

--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { Subscriber } = require('../models/Subscriber');
-const { auth } = require('../middleware/auth');
-const path = require('path');
 
 //=================================
 //             Subscriber
